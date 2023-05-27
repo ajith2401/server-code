4c08b8dd9f8844bec5d72e09c5336c27964e7002
ajith <ajith24ram@gmail.com>
2023-05-27 14:24:29 +0530

    server issue 500 error fixng

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const commentsFilePath = path.join(__dirname, 'comments.json');
 
 app.post('/api/comments', (req, res) => {
   const { name, mail_id, content, date, writingId, writingTitle } = req.body;
@@ -18,7 +21,7 @@ app.post('/api/comments', (req, res) => {
   // Read the existing comments from the JSON file
   let comments = [];
   try {
-    const data = fs.readFileSync('comments.json', 'utf8');
+    const data = fs.readFileSync(commentsFilePath, 'utf8');
     comments = JSON.parse(data);
   } catch (err) {
     console.error('Error reading comments file:', err);
@@ -32,14 +35,13 @@ app.post('/api/comments', (req, res) => {
   // Write the updated comments back to the JSON file
   try {
     const jsonData = JSON.stringify(comments);
-    fs.writeFileSync('comments.json', jsonData, 'utf8');
+    fs.writeFileSync(commentsFilePath, jsonData, 'utf8');
     res.status(200).json({ message: 'Comment stored', comment: newComment });
   } catch (err) {
     console.error('Error writing comments file:', err);
     res.status(500).json({ message: 'Error writing comments file' });
   }
 });
-const path = require('path');
 
 // Endpoint to handle '/ajith'
 app.get('/ajith', (req, res) => {
@@ -47,12 +49,12 @@ app.get('/ajith', (req, res) => {
 });
 
 // Serve the static files
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
 const port = 3000; // or any other port you prefer
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-// hello
+
 module.exports = app;
