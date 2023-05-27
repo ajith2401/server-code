9f0023b8a55e17026b5e4661fa013a61ab35a7c3
ajith <ajith24ram@gmail.com>
2023-05-27 16:12:24 +0530

    added env file

--- server.js
+++ server.js
@@ -1,46 +1,37 @@
-const fs = require('fs');
 const express = require('express');
-const path = require('path');
+const mysql = require('mysql');
 const app = express();
+require('dotenv').config();
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
 
 app.use(express.json());
-// Update the comments file path to a temporary location
-const commentsFilePath = path.join(__dirname, 'tmp', 'comments.json');
+
 app.post('/api/comments', (req, res) => {
   const { name, mail_id, content, date, writingId, writingTitle } = req.body;
-  const newComment = {
-    name: name,
-    mail_id: mail_id,
-    content: content,
-    date: date,
-    writingId: writingId,
-    writingTitle: writingTitle,
-  };
 
-  // Read the existing comments from the JSON file
-  let comments = [];
-  try {
-    const data = fs.readFileSync(commentsFilePath, 'utf8');
-    comments = JSON.parse(data);
-  } catch (err) {
-    console.error('Error reading comments file:', err);
-    res.status(500).json({ message: 'Error reading comments file' });
-    return;
-  }
-
-  // Add the new comment to the array
-  comments.push(newComment);
-
-  // Write the updated comments back to the JSON file
-  try {
-    const jsonData = JSON.stringify(comments);
-    fs.writeFileSync(commentsFilePath, jsonData, 'utf8');
-    res.status(200).json({ message: 'Comment stored', comment: newComment });
-  } catch (err) {
-    console.error('Error writing comments file:', err);
-    res.status(500).json({ message: 'Error writing comments file' });
-    return;
-  }
+  // Execute the SQL query to insert the comment
+  pool.query(
+    'INSERT INTO comments (name, mail_id, content, date, writingId, writingTitle) VALUES (?, ?, ?, ?, ?, ?)',
+    [name, mail_id, content, date, writingId, writingTitle],
+    (error, results) => {
+      if (error) {
+        console.error('Error writing comment to MySQL:', error);
+        res.status(500).json({ message: 'Error writing comment to MySQL' });
+      } else {
+        const commentId = results.insertId;
+        res.status(200).json({ message: 'Comment stored', commentId });
+      }
+    }
+  );
 });
 
 module.exports = app;
